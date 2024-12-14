99a8f873d9ccd42d972035347b40e8598f6af315
jwk <green23616@gmail.com>
2024-12-14 14:08:39 +0900

    상태관리에 따른 UI 변화 Test - 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,18 @@ import { styleState } from './store/styleState';
 function App() {
   const [styleVal, setStyleVal] = useRecoilState(styleState);
 
-  const handleMouseOver = () => {
-    setStyleVal(0);
-  };
-  const handleMouseOver2 = () => {
-    setStyleVal(1);
-  };
-  const handleMouseOver3 = () => {
-    setStyleVal(2);
+  const handleMouseOver = (number: number) => {
+    setStyleVal(number);
   };
 
   useEffect(() => {
-    console.log(styleVal);
     const setTimer = setTimeout(() => {
-      setStyleVal(prev => (prev > 1 ? 0 : prev + 1));
+      setStyleVal(prev => (prev > 2 ? 0 : prev + 1));
     }, 2000);
     return () => {
       clearTimeout(setTimer);
     };
-  }, [styleVal]);
+  }, [styleVal, setStyleVal]);
 
   return (
     <div className={styles.history}>
@@ -36,19 +29,19 @@ function App() {
             className={`${styles.container__side__job} ${
               styleVal === 0 ? styles.active : styles.inactive
             }`}
-            onMouseEnter={handleMouseOver}
+            onMouseEnter={() => handleMouseOver(0)}
           >
             <h2>2014 - 2020</h2>
             <h3>삼성전자 서비스</h3>
             <h4>유통, 판매 및 서비스 업무</h4>
             <h3>ABC마트</h3>
-            <h4>세일즈 부매니저 직급 업무</h4>
+            <h4>세일즈 마케터 부매니저 업무</h4>
           </div>
           <div
             className={`${styles.container__side__job} ${
               styleVal === 1 ? styles.active : styles.inactive
             }`}
-            onMouseEnter={handleMouseOver2}
+            onMouseEnter={() => handleMouseOver(1)}
           >
             <h2>2023.06 - 11</h2>
             <h3>그린아트 아카데미</h3>
@@ -58,17 +51,27 @@ function App() {
             className={`${styles.container__side__job} ${
               styleVal === 2 ? styles.active : styles.inactive
             }`}
-            onMouseEnter={handleMouseOver3}
+            onMouseEnter={() => handleMouseOver(2)}
           >
             <h2>2023.11 - 2024.02</h2>
             <h3>낸드소프트 프로젝트 사업부</h3>
             <h4>부산은행 사내 메일링 시스템 유지보수 업무</h4>
+          </div>
+          <div
+            className={`${styles.container__side__job} ${
+              styleVal === 3 ? styles.active : styles.inactive
+            }`}
+            onMouseEnter={() => handleMouseOver(3)}
+          >
+            <h2>2024.08</h2>
+            <h3>Projects</h3>
           </div>
         </div>
         <div className={styles.container__main}>
           {styleVal === 0 && 'Samsung'}
           {styleVal === 1 && 'Green Art'}
           {styleVal === 2 && 'Nandsoft'}
+          {styleVal === 3 && 'Works'}
         </div>
       </div>
     </div>
